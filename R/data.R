25c30a88868ee61c645f95822ae098235403f107
Eli Rapoport <er398@gmail.com>
2017-07-31 15:07:54 -0400

    data for vignette analysis

---
+++ R/data.R
@@ -0,0 +1,37 @@
+#' CNA patient survival
+#' 
+#' A dataset containing ######## data for 276 patients.
+#' The variables are as follows:
+#' 
+#' @format A data frame with 276 rows and 3 variables:
+#' \describe{
+#'   \item{months}{months ##### (0.92--72.01)}
+#'   \item{status}{######## (0,1)}
+#'   \item{strat}{####### (mutated, not_mutated)}
+#'}
+"tcga_genes"
+
+
+#' Heart disease
+#' 
+#' A dataset containing #### data for 302 patients.
+#' The variables are as follows:
+#' 
+#' @format A data frame with 302 rows and 14 variables:
+#' \describe{
+#'   \item{age}{age of patient (29--77)}
+#'   \item{sex}{sex of patient (0 (#####),1 (#####))}
+#'   \item{cp}{####### (from 1 (worst####) to 4 (best####))}
+#'   \item{trestbps}{###### (94--200)}
+#'   \item{chol}{##### (126--564)}
+#'   \item{fbs}{##### (0,1)}
+#'   \item{restecg}{###### (0,2)}
+#'   \item{thalach}{#### (71-202)}
+#'   \item{exang}{##### (0,1)}
+#'   \item{oldpeak}{####### (0--6.2)}
+#'   \item{slope}{#### (1,2,3)}
+#'   \item{ca}{###### (1 to 5)}
+#'   \item{thal}{#### (1 to 4)}
+#'   \item{num}{#### (0,1)}
+#' }
+"heart_disease"
